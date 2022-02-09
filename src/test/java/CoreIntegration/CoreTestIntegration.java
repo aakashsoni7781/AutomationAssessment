66e952bfffe5a01a31d588671cdc3a3ec38e676a
aakashsoni7781 <71699315+aakashsoni7781@users.noreply.github.com>
2022-02-09 20:29:46 +0530

    Update CoreTestIntegration.java

--- src/test/java/CoreIntegration/CoreTestIntegration.java
+++ src/test/java/CoreIntegration/CoreTestIntegration.java
@@ -3,10 +3,31 @@
  */
 package CoreIntegration;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import com.swaglab.basic.Test;
+
 /**
  * @author aakash.soni
  *
  */
 public class CoreTestIntegration {
+	  protected WebDriver test;
+	    protected static final String URL = "https://www.saucedemo.com/";
 
+	    @BeforeClass
+	    public void openBrowser() {
+	    	System.setProperty("webdriver.chrome.driver",".\\driver\\chromedriver.exe");
+			test = new Test().getTest();
+	        test.get(URL);
+	        test.manage().window().maximize();
+	        test.manage().deleteAllCookies();
+	    }
+
+	    @AfterClass
+	    public void closeBrowser() {
+	        test.quit();
+	    }
 }
